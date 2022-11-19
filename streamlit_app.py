805878a94746bd17bc66585899d08cc596528eb7
carlosth-bision <118626143+carlosth-bision@users.noreply.github.com>
2022-11-19 12:45:41 -0500

    Update streamlit_app.py
    Neew items and emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit
 
-streamlit.title("My Parent's New Healthy Diner")
+streamlit.title("My Mom's New Healthy Diner")
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smootie')
-streamlit.text('Hard-Boiled Fre-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smootie')
+streamlit.text('🐔 Hard-Boiled Fre-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
